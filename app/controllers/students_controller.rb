4c12191612a7176ef6a4a19caa92be2d1da061ec
Aaron Berns <anberns@gmail.com>
2019-03-19 17:05:17 -0400

    add crud to students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,37 @@
 class StudentsController < ApplicationController
+  def show
+    student = Student.find(params[:id])
+    render json: student 
+  end
+
+  def create 
+    student = Student.create(student_params)
+    render json: student
+  end
+
+  def edit
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  def update
+    student = Student.find(params[:id])
+    student.update(student_params)
+    render json: student
+  end
+
+  def index
+    #if params[:team_id]
+      #users = Team.find(params[:team_id]).users
+    #else
+    students = Students.all 
+    #end
+    render json: students
+  end
+
+  private
+
+    def student_params
+      params.permit(:name, :stage)
+    end
 end
