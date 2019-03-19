574d9b231bf06e9cd45bfdf19ac42cd0e48fee7f
Aaron Berns <anberns@gmail.com>
2019-03-19 17:02:14 -0400

    add crud to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create 
+    user = User.create(user_params)
+    render json: user
+  end
+
+  def edit
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    render json: user 
+  end
+
+  def index
+    #if params[:team_id]
+      #users = Team.find(params[:team_id]).users
+    #else
+    users = Users.all 
+    #end
+    render json: users
+  end
+
+  private
+
+    def user_params
+      params.permit(:name, :email, :password)
+    end
 end
