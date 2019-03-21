7f4036d6f3582e5ab03623e8bf1930f10c306d52
Aaron Berns <anberns@gmail.com>
2019-03-21 08:33:17 -0400

    fix phoneme serializer typo

--- app/serializers/phoneme_serializer.rb
+++ app/serializers/phoneme_serializer.rb
@@ -1,5 +1,5 @@
 class PhonemeSerializer < ActiveModel::Serializer
   attributes :id, :characters, :classification, :students
   has_many :student_phonemes
-  has_many :students, through: student_phonemes
+  has_many :students, through: :student_phonemes
 end
