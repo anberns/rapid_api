cb36335c7179228182c5da919325fe325ac99bc4
Aaron Berns <anberns@gmail.com>
2019-03-21 10:43:30 -0400

    turn off authenticity for json requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :student_phonemes
-  resources :phonemes, only: [:index] 
+  resources :phonemes 
   resources :students
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
