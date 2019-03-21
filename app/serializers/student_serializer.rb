449157c43c485a9afa0fa6bf8ab309ff082143d3
Aaron Berns <anberns@gmail.com>
2019-03-21 08:21:58 -0400

    add associations to student serializer

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,3 +1,6 @@
 class StudentSerializer < ActiveModel::Serializer
   attributes :id, :name, :stage
+  belongs_to :user
+  has_many :student_phonemes
+  has_many :phonemes, through: :student_phonemes
 end
