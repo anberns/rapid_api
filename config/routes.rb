9082ef85d2e6b887b5287cf7be728ae7af4b6a05
Aaron Berns <anberns@gmail.com>
2019-04-25 13:27:35 -0400

    add auth/login route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :students
   resources :users
   post 'auth/register', to: 'users#create'
+  post 'auth/login', to: 'users#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
