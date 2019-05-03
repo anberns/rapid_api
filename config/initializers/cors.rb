299a220ea184c738974248cef29f870d8ea57c68
Aaron Berns <anberns@gmail.com>
2019-05-03 10:27:35 -0400

    add heroku url to CORS list

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'http://localhost:8000'
+    origins 'localhost:3000', 'http://localhost:8000', 'https://rapid-exchange.herokuapp.com'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
